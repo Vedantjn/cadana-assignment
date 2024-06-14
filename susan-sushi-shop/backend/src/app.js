704d8288a21d614ff8894d343ec0b3927b0bcf9b
Vedantjn <vedant.jain.indore@gmail.com>
2024-06-14 17:04:49 +0530

    removed db.js

--- susan-sushi-shop/backend/src/app.js
+++ susan-sushi-shop/backend/src/app.js
@@ -11,12 +11,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 app.use(express.json());
 app.use('/api', orderRoutes);
