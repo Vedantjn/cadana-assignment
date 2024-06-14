4c20e83da81031b9bf1306ffcbd7b156b6dd28c3
Vedantjn <vedant.jain.indore@gmail.com>
2024-06-14 17:31:09 +0530

    updated app.js

--- susan-sushi-shop/backend/src/app.js
+++ susan-sushi-shop/backend/src/app.js
@@ -8,19 +8,15 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+app.use(cors());
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
   console.log('Connected to MongoDB');
 })
 .catch((err) => {
   console.error('Error connecting to MongoDB:', err);
 });
-
 
 app.use(express.json());
 app.use('/api', orderRoutes);
